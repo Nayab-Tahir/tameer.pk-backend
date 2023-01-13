50d3e77b19d22a1f7a39b98033a147f9b0ea461a
abdullahgr <aliabdullah292631@gmail.com>
2023-01-14 00:07:39 +0500

    user model specification.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MONGODB_CONNECTION_STRING } from './constants';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(MONGODB_CONNECTION_STRING), UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/user/schema/user.schema.ts
@@ -0,0 +1,27 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema()
+export class User {
+  @Prop()
+  name: string;
+
+  @Prop()
+  username: string;
+
+  @Prop()
+  email: string;
+
+  @Prop()
+  password: string;
+
+  @Prop()
+  phone: string;
+
+  @Prop()
+  address?: any;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
